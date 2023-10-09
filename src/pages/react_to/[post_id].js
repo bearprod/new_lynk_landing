578144539b1bfc4dcf03a19467d1bed3d6358f6b
abearducci <bearprod2022@gmail.com>
2023-10-09 15:35:32 -0400

    head

--- src/pages/react_to/[post_id].js
+++ src/pages/react_to/[post_id].js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { ref, getDownloadURL } from 'firebase/storage';
 import { storage } from '../../lib/firebase'; 
+import Head from 'next/head';  // <-- Importing the Head component
 
 export default function ReactToPost() {
   const router = useRouter();
@@ -25,22 +26,37 @@ export default function ReactToPost() {
   }, [post_id]);
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'black'
-    }}>
-      <p style={{ color: 'white', marginBottom: '1rem' }}>Post ID: {post_id}</p>
-      {imgURL ? (
-        <img src={imgURL} alt={`Shared Post ${post_id}`} style={{ maxWidth: '100%', maxHeight: '80vh' }} />
-      ) : (
-        <p style={{ color: 'white' }}>Loading...</p>
-      )}
-    </div>
+    <>
+      <Head>
+        <title>React to my post</title>
+        <meta name="description" content="View and react to the shared post." />
+
+        {/* Open Graph Protocol tags */}
+        <meta property="og:title" content="React to my post" />
+        <meta property="og:description" content="View and react to the shared post." />
+        <meta property="og:type" content="website" />
+        {imgURL && <meta property="og:image" content={imgURL} />}
+        <meta property="og:url" content={`https://lynkapp.co/react_to/${post_id}`} />
+      </Head>
+
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black'
+      }}>
+        <p style={{ color: 'white', marginBottom: '1rem' }}>Post ID: {post_id}</p>
+        {imgURL ? (
+          <img src={imgURL} alt={`Shared Post ${post_id}`} style={{ maxWidth: '100%', maxHeight: '80vh' }} />
+        ) : (
+          <p style={{ color: 'white' }}>Loading...</p>
+        )}
+      </div>
+    </>
   );
 }
+
 
 
